b4dc8e18db9cfa29b593d6143a143c3fc40c96e0
shohily <shohely.haque@gmail.com>
2015-06-03 09:52:06 -0700

    Student Info

---
+++ StudentInformationApp/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace StudentInformationApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rollTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.dateTextBox = new System.Windows.Forms.TextBox();
+            this.fatherNameTextBox = new System.Windows.Forms.TextBox();
+            this.motherNameTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Roll :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 21);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(344, 21);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Date of Birth :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 67);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Father Name : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(262, 67);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Mother Name :";
+            // 
+            // rollTextBox
+            // 
+            this.rollTextBox.Location = new System.Drawing.Point(53, 17);
+            this.rollTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.rollTextBox.Name = "rollTextBox";
+            this.rollTextBox.Size = new System.Drawing.Size(64, 23);
+            this.rollTextBox.TabIndex = 5;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(176, 17);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(162, 23);
+            this.nameTextBox.TabIndex = 6;
+            // 
+            // dateTextBox
+            // 
+            this.dateTextBox.Location = new System.Drawing.Point(422, 17);
+            this.dateTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.dateTextBox.Name = "dateTextBox";
+            this.dateTextBox.Size = new System.Drawing.Size(114, 23);
+            this.dateTextBox.TabIndex = 7;
+            // 
+            // fatherNameTextBox
+            // 
+            this.fatherNameTextBox.Location = new System.Drawing.Point(95, 63);
+            this.fatherNameTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.fatherNameTextBox.Name = "fatherNameTextBox";
+            this.fatherNameTextBox.Size = new System.Drawing.Size(156, 23);
+            this.fatherNameTextBox.TabIndex = 8;
+            // 
+            // motherNameTextBox
+            // 
+            this.motherNameTextBox.Location = new System.Drawing.Point(344, 63);
+            this.motherNameTextBox.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.motherNameTextBox.Name = "motherNameTextBox";
+            this.motherNameTextBox.Size = new System.Drawing.Size(192, 23);
+            this.motherNameTextBox.TabIndex = 9;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView.Location = new System.Drawing.Point(15, 155);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(521, 161);
+            this.dataGridView.TabIndex = 10;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Roll";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Name";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Date Of Birth";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Father Name";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Mother Name";
+            this.Column5.Name = "Column5";
+            // 
+            // insertButton
+            // 
+            this.insertButton.Font = new System.Drawing.Font("Segoe Marker", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertButton.Location = new System.Drawing.Point(95, 106);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(189, 32);
+            this.insertButton.TabIndex = 11;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // showButton
+            // 
+            this.showButton.Font = new System.Drawing.Font("Segoe Marker", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showButton.Location = new System.Drawing.Point(229, 335);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(109, 33);
+            this.showButton.TabIndex = 12;
+            this.showButton.Text = "Show Info";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("Segoe Marker", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchButton.Location = new System.Drawing.Point(323, 106);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(172, 32);
+            this.searchButton.TabIndex = 13;
+            this.searchButton.Text = "Search By Roll";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(546, 375);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.motherNameTextBox);
+            this.Controls.Add(this.fatherNameTextBox);
+            this.Controls.Add(this.dateTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.rollTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe Marker", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Student Information";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox rollTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox dateTextBox;
+        private System.Windows.Forms.TextBox fatherNameTextBox;
+        private System.Windows.Forms.TextBox motherNameTextBox;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ StudentInformationApp/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentInformationApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string connectionstring = @"server =USER;database=DB_StudentInfo;integrated security=true";
+        public int roll;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            roll = Convert.ToInt16(rollTextBox.Text);
+            string name = nameTextBox.Text;
+            string dateOfBirth = dateTextBox.Text;
+            string fatherName = fatherNameTextBox.Text;
+            string motherName = motherNameTextBox.Text;
+            SqlConnection aConnection=new SqlConnection(connectionstring);
+            aConnection.Open();
+            string query = string.Format(@"insert into StudentInfo_Table(Roll,Name,DateOfBirth,FatherName,MotherName)
+                   values('{0}','{1}','{2}','{3}','{4}')", roll, name, dateOfBirth, fatherName, motherName);
+               SqlCommand aCommand=new SqlCommand(query,aConnection);
+               MessageBox.Show("Data has been inserted");
+            rollTextBox.Text = "";
+            nameTextBox.Text = "";
+            dateTextBox.Text = "";
+            fatherNameTextBox.Text = "";
+            motherNameTextBox.Text = "";
+            aCommand.ExecuteNonQuery();
+            aConnection.Close();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            
+            dataGridView.Rows.Clear();
+            dataGridView.Refresh();
+            SqlConnection aConnection = new SqlConnection(connectionstring);
+            aConnection.Open();
+
+            string tableSql = "SELECT * FROM StudentInfo_Table";
+            SqlCommand selectAllTableSqlCommand = new SqlCommand(tableSql, aConnection);
+
+            SqlDataReader tableReader = selectAllTableSqlCommand.ExecuteReader();
+
+            if (tableReader != null)
+                while (tableReader.Read())
+                {
+                    dataGridView.Rows.Add(tableReader[0].ToString(), tableReader[1].ToString(),
+                                                 tableReader[2].ToString(), tableReader[3].ToString(),
+                                                 tableReader[4].ToString());
+                }
+            tableReader.Close();
+            aConnection.Close();
+ 
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+            dataGridView.Refresh();
+            SqlConnection aConnection = new SqlConnection(connectionstring);
+            aConnection.Open();
+
+            roll = Convert.ToInt16(rollTextBox.Text);
+
+            SqlCommand searchStudentByroll=
+                new SqlCommand("SELECT * FROM StudentInfo_Table WHERE Roll like '" + roll + "'", aConnection);
+
+            SqlDataReader tableReader = searchStudentByroll.ExecuteReader();
+            if (tableReader != null)
+            {
+                bool check = true;
+                while (tableReader.Read())
+                {
+                    dataGridView.Rows.Add(tableReader[0].ToString(), tableReader[1].ToString(),
+                                                tableReader[2].ToString(), tableReader[3].ToString(),
+                                                tableReader[4].ToString());
+                    check = false;
+                }
+                if (check)
+                { MessageBox.Show("Your desired Roll information is not present in the database. "); }
+                tableReader.Close();
+                aConnection.Close();
+            }
+        }
+    }
+}
+
+        
